45c11b862717d9e1d2a16dd8461b32f87e5024b6
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-06-27 20:41:41 +0530

    Add files via upload

---
+++ Basic - 1/_squareOfGivenNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+    cout << "Enter a number :" ;
+    cin >> x;
+    int y;
+    y = x*x;
+    cout << "Square of a number :" ;
+    cout << y;
+}
