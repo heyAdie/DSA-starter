21f01ec1004397981ddf0a5b4109285a50a0a87b
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-06-29 19:17:10 +0530

    Add files via upload

---
+++ Basic - 1/areaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main () 
+{
+    float radius ;
+    cout << " Enter Radius :";
+    cin >> radius;
+    
+    float a = 3.1415 * radius * radius;
+    cout << "Area Is : ";
+    cout << a ;
+}
