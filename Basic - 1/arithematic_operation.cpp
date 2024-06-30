9d909570079e1a1acdff7901ef50f98c19483e3d
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-06-30 21:45:33 +0530

    Add files via upload

---
+++ Basic - 1/arithematic_operation.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    int y = 2;
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x / y << endl;
+    cout << x % y << endl;
+    cout << x * y << endl;
+    cout << endl;
+
+     // increment and decrement :
+
+int p = 5;
+    cout<<p<<endl;
+    ++p;     // pre increment : x = x + 1
+    cout<<p<<endl;
+    p++;     // post increment :
+    cout<<p<<endl;
+    p--;     // x = x - 1
+    cout<<p<<endl;
+    --p;     // x = x - 1
+    cout<<p<<endl;
+    
+}
