d5554151b4cda7712dee7b430cd1f50756e5543e
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-07-01 13:42:50 +0530

    Add files via upload

---
+++ Basic - 1/BoolenDataType.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x  = 3;
+    int y = 5 ;
+    cout << (x==y) << endl;
+
+    int a = 4;
+    int b = 4;
+    cout << (a==b) << endl;
+
+    int p = 10;
+    int q = 5;
+    cout << (p>q) << endl;
+
+    int i = 5;
+    int j = 9;
+    cout << (i!=j) << endl;
+} 
