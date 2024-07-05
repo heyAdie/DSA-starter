aaf418fe801f2493357925851939869b297c3793
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-07-05 18:08:19 +0530

    Add files via upload

---
+++ Basic - 1/float_datatype.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    float x = 5;
+    float y = 2;
+    cout << x / y << endl;
+    cout << y / x << endl;
+    cout << 5 / 2 << endl;
+    cout << 5.0 / 2 << endl;
+}
