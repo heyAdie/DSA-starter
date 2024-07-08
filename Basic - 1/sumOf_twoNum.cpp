c55c8c46ab849df6278ccd3459f7c4a469229b69
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-07-08 20:13:31 +0530

    Add files via upload

---
+++ Basic - 1/sumOf_twoNum.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main () 
+{
+    int x ;
+    cout << "Enter first number : ";
+    cin >> x ;
+
+    int y ;
+    cout << "Enter second number : ";
+    cin >> y ;
+
+    cout << "Sum of two number : ";
+    cout << x + y ;
+    return 0;
+
+}
