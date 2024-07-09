c15d3f51d5dd1480dc1585142edf75db2ed55816
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-07-09 22:19:11 +0530

    Add files via upload

---
+++ Basic - 1/variable_declaration.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+    cout<<"heloow Adish.war"<<endl;
+    cout<<"heloow Raj"<<endl;
+    cout<<"heloow How's Going"<<endl;
+
+    int x ;
+    x = 7;
+    cout<<x<<endl;
+
+    int y;
+     y = 6;
+     y = 8;
+     y = 9;
+    cout << y << endl << endl;
+
+    int z ;
+    z = 4 ;
+    cout << z << endl ;
+    z = 5 ;
+    cout << z << endl;
+    z = 2 ;
+    cout << z << endl << endl;
+
+    int q ;
+    q = 19 ;
+    cout << q << endl << endl;
+    
+    q = q + 10 ;
+    cout << q ;
+}
