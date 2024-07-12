9167aecbefbd993b31fa46ee70c34f89d1635ff3
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-07-12 22:27:53 +0530

    Add files via upload

---
+++ Condition - 2/_1_if.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Enter a number : ";
+    int n;
+    cin >> n;
+    if( n % 2 == 0) {
+        cout <<"Even ";
+    } 
+    if ( n % 2 != 0) {
+        cout << "Odd";
+    } 
+    return 0;
+
+}
