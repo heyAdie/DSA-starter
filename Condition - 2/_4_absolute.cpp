7734d6035aa9ed4f4439ca5d98f7d6ae845e98b6
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-07-15 00:51:11 +0530

    Add files via upload

---
+++ Condition - 2/_4_absolute.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+ using namespace std;
+ int main() {
+    cout << "Enter a number : ";
+    int n;
+    cin >> n;
+    if (n > 0) {
+        cout << n ;
+    } else {
+        cout << -n;
+    }
+    return 0;
+ }
