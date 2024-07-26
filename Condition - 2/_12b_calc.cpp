f315b323d4321925b39c77deed5b902fa964045e
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-07-26 22:55:06 +0530

    Add files via upload

---
+++ Condition - 2/_12b_calc.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+int main () {
+    int a, b;
+    char op;
+    cout <<" Enter the problem : ";
+    cin >>a>>op>>b;
+    switch(op) {
+        case '+' :
+        cout << a+b ; 
+        break;     
+        case '-' :
+        cout << a-b ; 
+        break;
+        case '*' :
+        cout << a*b ; 
+        break;
+        case '/' :
+        cout << a/b ; 
+        break;  
+    }
+}
