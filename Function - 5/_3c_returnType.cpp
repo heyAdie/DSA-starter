f664350d40a424dcb31da76ed765609f16f3031d
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-08-11 01:05:18 +0530

    Add files via upload

---
+++ Function - 5/_3c_returnType.cpp
@@ -0,0 +1,17 @@
+
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int sum(int x, int y){
+    return x+y;
+}
+int mini(int x, int y){
+    int a;
+    if(x<y) a = x;
+    else a = y;
+    return a;
+}
+int main() {
+    cout << sqrt(9.7);
+}
