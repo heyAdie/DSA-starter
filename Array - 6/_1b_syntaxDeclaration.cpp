049f24dbf73666e80a1b4b6f66794aeeacf8f3c4
Adishwar Raj <149300476+heyAdie@users.noreply.github.com>
2024-08-23 21:19:36 +0530

    Add files via upload

---
+++ Array - 6/_1b_syntaxDeclaration.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main () {
+
+    // input________________
+    int arr[7];
+    for( int i=0; i<=6; i++){
+        cin >> arr[i] ;
+    }
+
+    //output_________________
+    for( int i=0; i<=6; i++){
+        cout << arr[i] << " ";
+    }
+}
